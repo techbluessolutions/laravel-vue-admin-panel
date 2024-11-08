40c20bdac5fd51f32953a98492250f6624322216
balajidharma <balajidharma@hotmail.com>
2024-11-07 20:01:45 -0500

    Added laravel category trait

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use BalajiDharma\LaravelMenu\Traits\LaravelCategories;
 use Spatie\Permission\Models\Permission as OriginalPermission;
 
 class Permission extends OriginalPermission
 {
+    use LaravelCategories;
+
     protected $fillable = [
         'name',
         'guard_name',
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use BalajiDharma\LaravelMenu\Traits\LaravelCategories;
 use Spatie\Permission\Models\Role as OriginalRole;
 
 class Role extends OriginalRole
 {
+    use LaravelCategories;
+
     protected $fillable = [
         'name',
         'guard_name',
